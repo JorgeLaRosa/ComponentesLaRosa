0d30c488b023fa6a5eb8aff16ae1bc3aa6ef948b
JorgeLaRosa <jorgem.larosa@gmail.com>
2022-03-20 18:29:38 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { MenuComponent } from './components/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostsComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule
